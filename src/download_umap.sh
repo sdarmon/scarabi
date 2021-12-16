5809c4e7de764a4a4e0a6b0637d3989463969915
sdarmon <sasha.darmon@ens-lyon.fr>
2021-12-16 13:06:07 +0000

    les sripts enfin dans le bon dossier!

---
+++ src/download_umap.sh
@@ -0,0 +1,11 @@
+cd ~/mydatalocal/scarabi/data
+
+mkdir -p ~/mydatalocal/scarabi/data/Counts_Salmon
+
+wget -r --user=tp_ngs --password='Arabido2021!' "https://flower.ens-lyon.fr/tp_ngs/scarabi/Li2016/Salmon/Counts_Salmon" --directory-prefix ~/mydatalocal/scarabi/data/Counts_Salmon/
+
+mv ~/mydatalocal/scarabi/data/Counts_Salmon/flower.ens-lyon.fr/tp_ngs/scarabi/Li2016/Salmon/Counts_Salmon/* ~/mydatalocal/scarabi/data/Counts_Salmon
+
+rm -r ~/mydatalocal/scarabi/data/Counts_Salmon/flower.ens-lyon.fr
+
+wget --user=tp_ngs --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/scarabi/Li2016/metadata_Li2016.txt -O ~/mydatalocal/scarabi/data/Counts_Salmon/metadata_Li2016.txt
